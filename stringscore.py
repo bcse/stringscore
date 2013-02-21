0a20d5ababf12be455e95a3008327d9e39e8ccdf
Grey Lee <grey@bcse.tw>
2013-02-21 12:19:12 +0800

    Porting iterative version from https://github.com/shelhamer/liquidmetal/tree/2df1659395c66cc7422ab8d9ad625cf809ef5251

--- stringscore.py
+++ stringscore.py
@@ -32,8 +32,7 @@ def score(string, abbrev):
         return SCORE_TRAILING
 
     # match & score all
-    all_scores = []
-    _score_all(string, string.lower(), abbrev.lower(), -1, 0, [None] * len(string), all_scores)
+    all_scores = _score_all(string, string.lower(), abbrev.lower())
 
     # complete miss
     if len(all_scores) == 0:
@@ -56,53 +55,65 @@ def score(string, abbrev):
     return max_score
 
 
-def _score_all(string, search, abbrev, search_index, abbr_index, scores, all_scores):
-    # save completed match scores at end of search
-    if abbr_index == len(abbrev):
-        # add trailing score for the remainder of the match
-        started = search[0] == abbrev[0]
-        trail_score = SCORE_TRAILING_BUT_STARTED if started else SCORE_TRAILING
-        if None in scores:
-            begin = scores.index(None)
-            end = len(scores) - scores[::-1].index(None)
-            fill_array(scores, trail_score, begin, end)
-        # save score clone (since reference is persisted in scores)
-        all_scores.append(scores[:])
-        return
+def _score_all(string, search, abbrev):
+    all_scores = []  # collection of scores for complete matches
+    scores = [None] * len(string)  # per-char scores, for accumulating complete scores
 
-    # consume current char to match
-    c = abbrev[abbr_index]
-    abbr_index += 1
+    # initialize the search for the first char at the beginning
+    # abbr_indices: index of the abbrev char to match
+    # search_indices: starting index for search string match
+    # fill_indices: index from which to fill-in score updates
+    match_abbr_indices = [0]
+    match_search_indices = [-1]
+    match_fill_indices = [0]
 
-    # cancel match if a character is missing
-    try:
-        index = search.index(c, search_index + 1)
-    except ValueError:
-        return
+    # match & score all sequences of the abbrev chars
+    while (len(match_abbr_indices) > 0):
+        abbr_index = match_abbr_indices.pop()
+        search_index = match_search_indices.pop()
+        fill = match_fill_indices.pop()
 
-    # match all instances of the abbreviaton char
-    score_index = search_index  # score section to update
-    while True:
+        # cancel match if char is missing
         try:
-            index = search.index(c, search_index + 1)
+            index = search.index(abbrev[abbr_index], search_index + 1)
         except ValueError:
-            break
+            continue
 
         # score this match according to context
         if is_new_word(string, index):
             if index > 0:
                 scores[index - 1] = SCORE_MATCH
-                fill_array(scores, SCORE_BUFFER, score_index + 1, index - 1)
+                fill_array(scores, SCORE_BUFFER, fill, index - 1)
         elif is_upper_case(string, index):
-            fill_array(scores, SCORE_BUFFER, score_index + 1, index)
+            fill_array(scores, SCORE_BUFFER, fill, index)
         else:
-            fill_array(scores, SCORE_NO_MATCH, score_index + 1, index)
-
+            fill_array(scores, SCORE_NO_MATCH, fill, index)
         scores[index] = SCORE_MATCH
 
-        # consume matched string and continue search
-        search_index = index
-        _score_all(string, search, abbrev, search_index, abbr_index, scores, all_scores)
+        # queue up next search for this char
+        match_abbr_indices.append(abbr_index)
+        match_search_indices.append(index)
+        match_fill_indices.append(fill)
+
+        # continue search or complete match and score
+        if abbr_index != len(abbrev) - 1:
+            # continue search, advancing to next abbrev char
+            match_abbr_indices.append(abbr_index + 1)
+            match_search_indices.append(index)
+            match_fill_indices.append(index + 1)
+        else:
+            # all abbrev chars have been matched, complete score and record
+            # add trailing score for the remainder of the match
+            started = (search[0] == abbrev[0])
+            trail_score = SCORE_TRAILING_BUT_STARTED if started else SCORE_TRAILING
+            if None in scores:
+                begin = scores.index(None)
+                end = len(scores) - scores[::-1].index(None)
+                fill_array(scores, trail_score, begin, end)
+            # save score clone (since reference is persisted in scores)
+            all_scores.append(scores[:])
+
+    return all_scores
 
 
 def is_upper_case(string, index):

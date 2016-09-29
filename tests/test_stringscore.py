0a598ccb662f133dc632545cb35152c56742216c
Grey Lee <bcse@bcse.tw>
2016-09-29 16:43:19 +0800

    Try to fix Travis CI. Try to merge them into one file...

--- tests/test_stringscore.py
+++ tests/test_stringscore.py
@@ -10,6 +10,9 @@ Tests for `stringscore` module.
 
 import unittest
 
+from hypothesis import given, settings
+from hypothesis import strategies
+
 from stringscore import liquidmetal
 from stringscore import quicksilver
 
@@ -105,6 +108,24 @@ class QuickSilverTestCase(unittest.TestCase):
 
         for k, v in tests:
             self.assertEqual(round(quicksilver.score(*k), 12), v)
+
+
+class LiquidMetalHypothesisTestCase(unittest.TestCase):
+
+    @given(strategies.text(), strategies.text())
+    @settings(max_examples=50000)
+    def test_score_boundaries(self, string, abbrev):
+        score = liquidmetal.score(string, abbrev)
+        self.assertTrue(0.0 <= score <= 1.0)
+
+
+class QuickSilverHypothesisTestCase(unittest.TestCase):
+
+    @given(strategies.text(), strategies.text())
+    @settings(max_examples=50000)
+    def test_score_boundaries(self, string, abbrev):
+        score = quicksilver.score(string, abbrev)
+        self.assertTrue(0.0 <= score <= 1.0)
 
 
 if __name__ == '__main__':
